95acd8159a6c7692ea87d25042495c48945282f7
Young T8 <victor.veltan@fortech.ro>
2021-10-03 20:12:26 +0300

    DADA

--- src/index.js
+++ src/index.js
@@ -8,10 +8,8 @@ app.get('/', function(req,res){
     res.sendFile('globe.html', {root:__dirname});
 });
 
-app.get('/getCZML', async function(req,res){
+app.get('/getCZML',  function(req,res){
     res.sendFile(path.resolve(__dirname,"../data/newCZML.czml"));
-    //const result =  await axios.get("localhost/test")
-    //res.send(result);
 });
 
 app.listen(process.env.PORT || 3000);
