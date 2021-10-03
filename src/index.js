6634efad4497626d06408b9e58066106af65e8e6
Young T8 <victor.veltan@fortech.ro>
2021-10-03 15:09:55 +0300

    dad

--- src/index.js
+++ src/index.js
@@ -8,7 +8,34 @@ app.get('/', function(req,res){
 });
 
 app.get('/getCZML', function(req,res){
+    res.sendFile(path.resolve(__dirname,"../data/newCzml.czml"));
+});
+
+app.get('/getCZML1', function(req,res){
     res.sendFile(path.resolve(__dirname,"../data/orbit.czml"));
 });
 
+app.get('/getCZML2', function(req,res){
+    res.sendFile(path.resolve(__dirname,"../data/orbit.czml"));
+});
+
+app.get('/getCZML3', function(req,res){
+    res.sendFile(path.resolve(__dirname,"../data/orbit.czml"));
+});
+
+app.get('/getCZML4', function(req,res){
+    res.sendFile(path.resolve(__dirname,"../data/orbit.czml"));
+});
+
+app.get('/getCZML5', function(req,res){
+    res.sendFile(path.resolve(__dirname,"../data/orbit.czml"));
+});
+
+app.get('/getCZML6', function(req,res){
+    res.sendFile(path.resolve(__dirname,"../data/orbit.czml"));
+});
+
+app.get('/getCZML7', function(req,res){
+    res.sendFile(path.resolve(__dirname,"../data/orbit.czml"));
+});
 app.listen(process.env.PORT || 3000);
